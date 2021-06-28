6abeb1c3a35e9f3c9a46952b210fa65680cf8876
Jonathon Chambers <jonathon@jonathonchambers.com>
2021-06-27 20:52:34 -0400

    Updated Site for Initial Build

--- Components/Header/index.js
+++ Components/Header/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Header () {
     return (
         <div className={styles.Header}>
-            <div><a href="/">Jonathon Chambers</a></div>
+            <div><Link href="/">Jonathon Chambers</Link></div>
             <div>
                 <ul className={styles.navItems}>
                     <li><a href="#about">About</a></li>
