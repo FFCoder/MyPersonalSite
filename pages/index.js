1b4ac520ab311ed3f48ea3bc531ccac9554aba75
Jonathon Chambers <FFCoder@users.noreply.github.com>
2021-07-09 01:25:30 -0400

    Added WIP Banner

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import styles from '../styles/Home.module.css'
 import Header from '../Components/Header'
 import Main from "../Components/IndexMain"
+import { Alert } from 'react-bootstrap'
 
 export default function Home() {
   function HandleHeaderScroll(){
 
   }
   return (<div>
+    <Alert variant="danger">
+    <Alert.Heading>Attention: </Alert.Heading>
+      <p>
+        Hi!
+        This site is still a work in progress. Please don't judge too harshly.
+      </p>
+      <p>
+        As I add more content and I feel more comfortable presenting it, I will remove this banner</p> 
+      </Alert>
 
     <Header onScroll={HandleHeaderScroll} />
     <Main id="main" />
